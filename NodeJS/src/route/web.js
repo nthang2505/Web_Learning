c6109932e96e15fb83a9cf7564db54fbb20d1148
nthang2505 <nthang2505>
2022-06-21 14:32:36 +0700

    Apply MVC paradigm

--- NodeJS/src/route/web.js
+++ NodeJS/src/route/web.js
@@ -1,12 +1,11 @@
 import express from "express";
+import homeController from "../controllers/home.controller"
 
 let router = express.Router();
 
 let initWebRouter = (app) => {
-    router.get("/", (req, res)=>{
-        return res.send("Hello world!!!")
-    });
-
+    router.get("/", homeController.getHomePage);
+    router.get("/about", homeController.getAboutPage);
     return app.use("/", router)
 }
 
